df1aaa7b971a87e1e1ba1d957f436aededf04c55
Nicolomecca <nico97.mecca@gmail.com>
2024-11-08 17:23:25 +0100

    modifica del filtro

--- Progetto_W6/src/main/java/Nicolo_Mecca/Progetto_W6/security/JWTCheckerFilter.java
+++ Progetto_W6/src/main/java/Nicolo_Mecca/Progetto_W6/security/JWTCheckerFilter.java
@@ -1,19 +1,13 @@
 package Nicolo_Mecca.Progetto_W6.security;
 
-import Nicolo_Mecca.Progetto_W6.entities.User;
 import Nicolo_Mecca.Progetto_W6.excepetions.UnauthorizedException;
-import Nicolo_Mecca.Progetto_W6.services.UserService;
 import Nicolo_Mecca.Progetto_W6.tools.JWT;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -23,30 +17,32 @@ public class JWTCheckerFilter extends OncePerRequestFilter {
     @Autowired
     private JWT jwt;
 
-    @Autowired
-    private UserService userService;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
 
-        if (authHeader == null || !authHeader.startsWith("Bearer "))
+        if (authHeader == null && isPublicGetEndpoint(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             throw new UnauthorizedException("Inserire token nell'Authorization Header nel formato corretto!");
+        }
 
         String accessToken = authHeader.substring(7);
         jwt.verifyToken(accessToken);
 
-        String userId = jwt.getIdFromToken(accessToken);
-        User currentUser = this.userService.findById(Long.parseLong(userId));
-
-        Authentication authentication = new UsernamePasswordAuthenticationToken(currentUser, null, currentUser.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
         filterChain.doFilter(request, response);
     }
 
+    private boolean isPublicGetEndpoint(HttpServletRequest request) {
+        return request.getMethod().equals("GET") &&
+                request.getRequestURI().startsWith("/events");
+    }
+
     @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return new AntPathMatcher().match("/auth/**", request.getServletPath());
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getRequestURI().startsWith("/auth/");
     }
 }
