c8ee7d3655e7d8d452bac0bda5ffe850d8d054db
Nicolomecca <nico97.mecca@gmail.com>
2024-11-08 10:22:59 +0100

    configurazione progetto

---
+++ Progetto_W6/src/main/java/Nicolo_Mecca/Progetto_W6/ProgettoW6Application.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.Progetto_W6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoW6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoW6Application.class, args);
+	}
+
+}
---
+++ Progetto_W6/src/test/java/Nicolo_Mecca/Progetto_W6/ProgettoW6ApplicationTests.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.Progetto_W6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoW6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
